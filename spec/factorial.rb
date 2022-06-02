ba2b46b8d32f769b1ef415f22134cf4563515b6b
Nicholas Mutale <nicomutale@gmail.com>
2022-06-02 12:49:05 +0200

    Add factorial tests

---
+++ spec/factorial.rb
@@ -0,0 +1,16 @@
+describe Solver do
+    before :each do
+        @solver = Solver.new
+    end
+    it 'accepts only positve int' do
+        expect(@solver.factorial(0)).to eq(1)
+        expect(@solver.factorial(-1)).to eq(nil)
+    end
+    it 'returns correct factorial' do
+        expect(@solver.factorial(1)).to eq(1)
+        expect(@solver.factorial(2)).to eq(2)
+        expect(@solver.factorial(3)).to eq(6)
+        expect(@solver.factorial(4)).to eq(24)
+        expect(@solver.factorial(5)).to eq(120)
+    end
+end
