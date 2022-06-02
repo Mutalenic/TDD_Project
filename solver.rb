b1ee3d5c86f5210638b1291648b80a2a20e87afa
Nicholas Mutale <nicomutale@gmail.com>
2022-06-02 14:56:59 +0200

    Fix linter error

--- solver.rb
+++ solver.rb
@@ -5,4 +5,18 @@ class Solver
 
     num * factorial(num - 1)
   end
+
+  def fizzbuzz(num)
+    if num.zero?
+      0
+    elsif num.divisible_by?(3) && num.divisible_by?(5)
+      'fizzbuzz'
+    elsif num.divisible_by?(3)
+      'fizz'
+    elsif num.divisible_by?(5)
+      'buzz'
+    else
+      num
+    end
+  end
 end
