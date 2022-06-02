d4d651ae63475f5f5c39ef7afd890900b06b1d32
Nicholas Mutale <nicomutale@gmail.com>
2022-06-02 18:27:05 +0200

    Correct tests

--- solver.rb
+++ solver.rb
@@ -1,22 +1,26 @@
 class Solver
   def factorial(num)
-    return nil if num.negative?
-    return 1 if num.zero?
-
-    num * factorial(num - 1)
+    if num < 0
+      nil
+    elsif num == 0
+      1
+    else
+      num * factorial(num - 1)
+    end
   end
-
+  def reverse(str)
+    str.reverse
+  end
   def fizzbuzz(num)
-    if num.zero?
-      0
-    elsif num.divisible_by?(3) && num.divisible_by?(5)
+    if num % 3 == 0 && num % 5 == 0
       'fizzbuzz'
-    elsif num.divisible_by?(3)
+    elsif num % 3 == 0
       'fizz'
-    elsif num.divisible_by?(5)
+    elsif num % 5 == 0
       'buzz'
     else
       num
     end
   end
+  
 end
