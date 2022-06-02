3193c2894201e18ebf29da51857382db3796546b
Nicholas Mutale <nicomutale@gmail.com>
2022-06-02 13:15:57 +0200

    Add tests for reverse and fizzbuzz method

---
+++ spec/reverse.rb
@@ -0,0 +1,12 @@
+describe  Solver do
+    before :each do
+        @solver = Solver.new
+    end
+    it 'When given a string, it should return the reverse of the string' do
+        expect(@solver.reverse("Hello")).to eq("olleH")
+    end
+    it 'When string is empty' do
+        expect(@solver.reverse("")).to eq("")
+    end
+    end
+    end
